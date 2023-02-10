73eae9721840d4fa8b24fcdf597c20fb007fa82d
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-02-10 17:13:45 -0300

    Modifying authentication route

--- models/User.js
+++ models/User.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema(
     {
-        email: { type: String, required: true, unique: true},
-        password: { type: String, required: true},
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        // role: { type: String, default: "Basic", required: true},
+        isAdmin: { type: Boolean, default: false },
     },
     { timestamps: true }
 )
